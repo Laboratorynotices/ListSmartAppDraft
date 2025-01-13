cfde479c10a8844f454ffa81c7fcbf306b4d0037
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-13 18:02:10 +0100

    feat(app): добавить лог данных текущего пользователя в консоль
    Добавлен вывод информации о текущем пользователе, полученной от useCurrentUser(), в консоль. Это изменение поможет отладке работы с данными пользователя.

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+    <h1>Check console.log() for test.</h1>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const user = useCurrentUser();
+
+console.log(user);
+</script>
