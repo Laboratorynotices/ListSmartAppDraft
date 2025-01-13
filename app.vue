8486ba7e746686885b2eb76f3f6eeb5b7bc2088f
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-13 20:09:55 +0100

    feat(app): добавлены кнопки для авторизации через Google
    Удалены ненужные элементы из файла app.vue. Добавлены кнопки, вызывающие функции loginWithGoogle и logoutWithGoogle.
    Функционал находится на стадии разработки и используется в учебных целях.

--- app.vue
+++ app.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
     <h1>Check console.log() for test.</h1>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="loginWithGoogle">Login with Google</button>
+    <br />
+    <button @click="logoutWithGoogle">Logout with Google</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 const user = useCurrentUser();
 
+// @TODO удалить
 console.log(user);
+
+// @TODO перенести в мидлваре
+const auth = useFirebaseAuth();
+
+const loginWithGoogle = () => {
+  // auth может быть null, что нам не подходит
+  if (auth) {
+    signInWithPopup(auth, new GoogleAuthProvider());
+  }
+};
+
+const logoutWithGoogle = () => {
+  auth?.signOut();
+};
 </script>
