f36b3d114a33d228781f906c1204c1f67045abe5
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-16 20:29:05 +0100

    feat(shopping-list): добавлены файлы хранилища и компонента для списка покупок
    - Добавлен файл `shoppingListStore.ts`, содержащий Pinia-хранилище для списка покупок с начальным состоянием, категориями и заготовками методов.
    - Добавлен файл `shoppingListCSR.vue`, пока содержащий тестовый компонент для проверки работы хранилища. Впоследствии будет интерфейсом для CSR.

---
+++ pages/shoppingListCSR.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hallo</h1>
+</template>
+
+<script setup lang="ts">
+import { useShoppingStore } from "@/stores/useShoppingStore";
+
+// Тест
+// Получаем доступ к хранилищу
+const shoppingStore = useShoppingStore();
+
+// Проверим содержимое
+console.log(shoppingStore);
+</script>
