42f66e2987e7b8f8fd2e3ed7f936c99385db35c9
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-16 05:49:47 +0100

    feat(auth, middleware): добавлена обработка перенаправления авторизованных пользователей
    Добавлена логика перенаправления авторизованных пользователей, пытающихся попасть на страницу логина, на главную страницу.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = await getCurrentUser();
 
+  // Если пользователь авторизован и открывает страницу авторизации, то перенаправляем на главную страницу
+  if (user && to.path === "/login") {
+    return navigateTo("/");
+  }
+
   // Если пользователь не авторизирован и мы ещё не направляемся на страницу входа
   if (!user && to.path !== "/login") {
     // перенаправляемся на страницу логина
