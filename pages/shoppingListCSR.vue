d0203c25af043e55e70191bda85a691719450b7b
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-17 21:54:06 +0100

    feat(shopping-list): добавить проверку авторизации для доступа к странице
    Теперь страница shoppingListCSR вызывает мидлваре auth, чтобы только авторизованные пользователи могли ею пользоваться.

--- pages/shoppingListCSR.vue
+++ pages/shoppingListCSR.vue
@@ -5,6 +5,11 @@
 <script setup lang="ts">
 import { useShoppingStore } from "@/stores/useShoppingStore";
 
+// Доступ только авторизованным пользователям
+definePageMeta({
+  middleware: ["auth"],
+});
+
 // Тест
 // Получаем доступ к хранилищу
 const shoppingStore = useShoppingStore();
