ee2258d687116ecfa7d178ca5c85aecb17f0f980
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-01-22 12:24:08 +0100

    feat(shopping-list, store): добавил storeHelpers и обновил обработку добавления элементов
    - В useShoppingStore.ts добавлен объект `storeHelpers` с новой "приватной" асинхронной функцией `addDocToFirebase()`, которая отвечает за добавление документа в Firebase.
    - Функция `addItem` теперь вызывает `addDocToFirebase()` и стала асинхронной.
    - В ShoppingItemForm.vue обновлён вызов функции `addItem` с учётом её асинхронности.

--- stores/useShoppingStore.ts
+++ stores/useShoppingStore.ts
@@ -1,3 +1,5 @@
+import { addDoc, collection, type DocumentData } from "firebase/firestore";
+import { useFirestore } from "vuefire";
 import { defineStore } from "pinia";
 
 // Определяем интерфейс для отдельного элемента списка покупок
@@ -18,6 +20,38 @@ interface ShoppingState {
   isLoading: boolean;
   error: string | null;
 }
+
+// Имя коллекции в Firebase
+const COLLECTION_NAME = "shoppingItems";
+
+/**
+ * Вспомогательные (приватные) методы для работы с Firebase
+ */
+const storeHelpers = {
+  // Сохранение данных в Firebase
+  async addDocToFirebase(
+    item: Omit<ShoppingItem, "id">
+  ): Promise<DocumentData> {
+    // Используем $db для доступа к базе данных
+    //const { $db } = useNuxtApp();
+    // Получаем доступ к базе данных через VueFire
+    const db = useFirestore();
+
+    try {
+      // Добавляем элемент в базу данных
+      // В первый раз ещё и создаст "коллекцию"
+      return await addDoc(
+        // авторизация и указание на "коллекцию"
+        collection(db, COLLECTION_NAME),
+        // данные для добавления
+        item
+      );
+    } catch (e) {
+      console.error("Ошибка при добавлении shoppingItem: ", e);
+      return { id: undefined };
+    }
+  },
+};
 
 // Создаём store с использованием Composition API стиля
 export const useShoppingStore = defineStore("shopping", {
@@ -44,21 +78,26 @@ export const useShoppingStore = defineStore("shopping", {
   // Actions - методы для изменения состояния
   actions: {
     // @TODO Добавление нового элемента в
-    addItem(item: Omit<ShoppingItem, "id" | "createdAt">) {
-      const newItem: ShoppingItem = {
-        // @TODO Временно, потом получим id от Firebase
-        id: new Date().toISOString(),
+    async addItem(item: Omit<ShoppingItem, "id" | "createdAt">) {
+      // @TODO проверить есть ли элемент с таким именем
+      // Создаём новый элемент
+      const newItem: Omit<ShoppingItem, "id"> = {
         createdAt: new Date(),
         ...item,
       };
-      this.items.push(newItem);
+
+      // Добавляем элемент в базу данных, получаем ID
+      // В первый раз ещё и создаст "коллекцию"
+      const docRef = await storeHelpers.addDocToFirebase(newItem);
+
+      // Добавляем элемент в список, добавляя ему ID
+      this.items.push({ id: docRef.id, ...newItem });
     },
     // @TODO Удаление элемента из списка
     // @TODO Обновление существующего элемента
     // @TODO Переключение статуса выполнения
     // @TODO Добавление новой категории
-    // @TODO Загрузка данных из локального хранилища
-    // @TODO Сохранение данных в локальное хранилище
+    // @TODO Загрузка данных из Firebase
     // @TODO Очистка списка
   },
 });
